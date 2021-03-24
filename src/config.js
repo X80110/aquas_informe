70d3d1d6e9fe87840689ce2a75fd95b630fa2f5b
Xavier B <72877008+X80110@users.noreply.github.com>
2021-03-24 01:49:09 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,75 @@
+// CORE CONFIG
+export const themes = {
+  'light': {
+    'text': '#222',
+    'muted': '#777',
+    'pale': '#f0f0f0',
+    'background': '#fff'
+  },
+  'dark': {
+    'text': '#fff',
+    'muted': '#bbb',
+    'pale': '#333',
+    'background': '#222'
+  }
+}
+
+// CONTENT CONFIG
+export const arees = [
+	{
+		code: '78',
+		name: 'Barcelona'
+	},
+	{
+		code: '71',
+		name: 'Alt Pirineu i Aran'
+	},
+	{
+		code: '67',
+		name: 'Catalunya Central'
+	},
+	{
+		code: '64',
+		name: 'Girona'
+	},
+	{
+		code: '63',
+		name: "Terres de l'Ebre"
+	},
+	{
+		code: '62',
+		name: 'Camp de Tarragona'
+	},
+	{
+		code: '61',
+		name: 'Lleida'
+	},
+	{
+		code: 'SE',
+		name: 'Sense Especificar'
+	},
+];
+
+export const colors = [
+	[32, 96, 149],
+	[39, 160, 204],
+	[0, 60, 87],
+	[17, 140, 123],
+	[168, 189, 58],
+	[135, 26, 91],
+	[246, 96, 104],
+	[116, 108, 177],
+	[34, 208, 182]
+];
+
+export const datakeys = {
+	pacients: 'Pacients',
+	visites: 'Visites',
+	sexe: 'Gènere',
+	tipus_centre: 'Tipus de centre',
+	grup_edat: "Grups d'edat",
+	pcsm: 'Pacient crònic',
+	pccsm: 'Pacient crònic complex',
+	any: 'Any',
+	
+};
